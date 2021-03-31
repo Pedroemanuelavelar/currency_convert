15c4235b8345f83cd1e132ee551b88f3b041bae6
Pedro Emanuel de Avelar Sousa de Almeida <pedroemanuel5296@gmail.com>
2021-03-31 16:25:46 -0300

    Adicionado api-de-moedas

--- js/app.js
+++ js/app.js
@@ -1,21 +1,29 @@
-// Função que ira converter a moeda
+// Função que ira converter a moeda + o request de uma api para pegar a conversão do dolar comercial
 function currencyConverter() {
-    var Dolar = 5.79;
     var currencyValueBRL = Number(document.querySelector('#container__input-brl').value);
-    currencyBRL = document.querySelector('#container__input-brl');
-    var convertedCurrency = 0;
-    var convertUSDtoBRL = currencyValueBRL;
 
-    /* var currencyTypeTo = document.querySelector('.currency-type-to').value; */
+    // URL com o tipo de moeda (Por enquanto só USD), para devolver a cotação
+    const urlApi = "https://economia.awesomeapi.com.br/json/USD-BRL";
 
-    convertedCurrency =  convertUSDtoBRL / Dolar;
+    // Realizando o request para a api [https://docs.awesomeapi.com.br/api-de-moedas]
+    const requestApi = new XMLHttpRequest();
+    requestApi.open('GET', urlApi, false);
+    requestApi.send();
 
-    console.log(convertedCurrency);
 
-    return document.querySelector('input.brl').innerHTML = convertedCurrency.toFixed(2);
+    // Tratando os dados para JSON
+    const responsedata = requestApi.responseText;
+    const responseApi = JSON.parse(responsedata);
+    console.log(responseApi);
+
+    var Dolar = `${Number(responseApi[0].low)}`;
+    /* console.log(Dolar); */
+
+
+    var convertedCurrency =  currencyValueBRL * Dolar;
+
+    var test = document.querySelector('.container__input-result');
+    test.value = convertedCurrency.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+
+    /* console.log(test); */
 }
-
-currencyConverter();
-
-
-
